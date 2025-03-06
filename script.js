37b799108d483fe0e7ec50d18ca669d132c8d4b4
Hicham-Azeroual <hichamazroual2002@gmail.com>
2025-03-06 13:31:36 +0000

    add the logic for the game

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+/* console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+ */
+
+// create a function to create a rondom value between 1 and 20
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
